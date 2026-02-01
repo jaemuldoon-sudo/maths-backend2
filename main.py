13e829ab187031533d64290d6bce03d19a0cb56a
Joe Muldoon (Nokia) <joe.muldoon@nokia.com>
2026-02-01 10:17:56 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ st.set_page_config(page_title="Maths Tutor Test", page_icon="📐")
 st.title("📐 Maths Tutor Backend Test")
 st.write("If you can see this running on Streamlit, everything is working!")
 
-name = st.text_input("Enter your name:")
+name = st.text_input("Enter your naaaaaaaaaame:")
 
 if st.button("Say hello"):
     st.success(f"Hello {name}, your backend is working!")
